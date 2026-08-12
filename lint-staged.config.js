750bdc3f71900db633fdb3eb44d68d078f52218f
Jonattan Velasquez <jonattanva89@gmail.com>
2026-08-12 09:02:19 -0500

    upgrade dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,10 +3,6 @@ module.exports = {
     'prettier --write',
     'eslint -c packages/luna-react/eslint.config.mjs --fix',
   ],
-  'packages/luna-svelte/**/*.{mjs,ts,tsx,svelte}': [
-    'prettier --write',
-    'eslint -c packages/luna-svelte/eslint.config.mjs --fix',
-  ],
-  '!(packages)/**/*.{mjs,ts,tsx}': ['prettier --write', 'eslint --fix'],
+  'packages/luna-core/**/*.{mjs,ts,tsx}': ['prettier --write', 'eslint --fix'],
   '*.{json,md,css,html,yml}': ['prettier --write'],
 }
